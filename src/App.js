3321770bfadd222981fb331c91fbc65fff362594
Blecht <przemyslawblecharz@gmail.com>
2017-02-11 16:43:03 +0100

    adding images placeholders

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 import { cardList } from './TypesAndDefaults'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      card: '',
-    }
-  }
-  
   render() {
     return (
-      <div className="App ochlapus">
+      <div className="App-ochlapus">
         <div className="card--data">
           <ul className="card-list">
             {cardList.map((card, index) =>
               <li key={index} className="card-list--item">
                 <span className="card-list--item--name">{card.name}</span>
-                <span className="card-list--item--picture">{card.picture}</span>
+                <img src={ logo } className="card-list--item--picture" alt="logo" />
                 <span className="card-list--item--effect">{card.effect}</span>
                 <span className="card-list--item--description">{card.description}</span>
                 <span className="card-list--item--filter">{card.filter}</span>
--- src/TypesAndDefaults.js
+++ src/TypesAndDefaults.js
@@ -1,21 +1,21 @@
 export const cardList = [
     {
         name: 'Card 1',
-        picture:'',
+        picture: '',
         effect: '+10 to STR',
         description: 'Such a power!',
         filter: 'Dwarf',
     },
     {
         name: 'Card 2',
-        picture:'',
+        picture: '',
         effect: '+20 to AGI',
         description: 'Wow wow, so quick!',
         filter: 'Human',
     },
     {
         name: 'Card 3',
-        picture:'',
+        picture: '',
         effect: 'None',
         description: 'As always you get nothin, such a loser',
         filter: 'Basic',
