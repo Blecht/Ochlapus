3d509bf149abb666ff8d20e5e51139a8b6d349b0
Blecht <przemyslawblecharz@gmail.com>
2017-02-14 14:17:39 +0100

    select race added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import beer from './Beer.png';
 import './App.css';
 import { cardList } from './TypesAndDefaults'
 
@@ -7,14 +7,33 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchFilter: '',
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
+      race: '',
+      characterName: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+  }
+
+    handleSubmit(event) {
+      this.setState(this.state.value);
   }
 
   handleInputChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    
+    this.setState({
+      [name]: value
+    });
   }
+
 
   render() {
     return (
@@ -22,14 +41,25 @@ class App extends Component {
         <div className="card--data">
          <div className="card--filter">
           <div className="card--filter-input">
-            <input className="filter-input" type="text" name="searchFilter" placeholder="Add filter..." value={this.state.searchFilter} onChange={this.handleInputChange}/>
+            <input className="filter-input" type="text" name="characterName" placeholder="Name here" value={this.state.characterName} onChange={this.handleInputChange}/>
           </div>
+         <form >
+          <label>
+            Add filter:
+            <select value={this.state.value} onChange={this.handleInputChange} name="race">
+              <option defaultValue="None">None</option>
+              <option value="Human">Human</option>
+              <option value="Elf">Elf</option>
+              <option value="Dwarf">Dwarf</option>
+            </select>
+          </label>      
+        </form>
          </div> 
           <ul className="cardList">
             {cardList.map((card, index) =>
               <li key={index} className="cardList--item">
                 <span className="cardList--item--name">{card.name}</span>
-                <img src={ logo } className="cardList--item--picture" alt="logo" />
+                <img src={ beer } className="cardList--item--picture" alt="logo" />
                 <span className="cardList--item--effect">{card.effect}</span>
                 <span className="cardList--item--description">{card.description}</span>
                 <span className="cardList--item--filter">{card.filter}</span>
